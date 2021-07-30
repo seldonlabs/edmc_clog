10f1314e7088887a7bf40681f49362528473fc64
Marcus Seldon <35172230+seldonlabs@users.noreply.github.com>
2021-07-30 13:19:49 +0200

    set timeout to 15 seconds

--- edmc_clog_net.py
+++ edmc_clog_net.py
@@ -2,11 +2,12 @@ from requests import get, ConnectTimeout, ConnectionError, HTTPError, ReadTimeou
 
 from edmc_clog_utils import APP_VER
 
+
 def call_service(pilot_name):
     try:
         r = get(f"http://roa.services.seldonlabs.com/?pattern={pilot_name}", headers={
           'User-Agent': f"EDMC-CLog-v-{APP_VER}"
-        }, timeout=5)
+        }, timeout=15)
 
         r.raise_for_status()
 
